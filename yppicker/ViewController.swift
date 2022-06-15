2001c75d5312e27c5e7c6a652937f178fdc1aca7
john-rocky <rockyshikoku@gmail.com>
2022-06-15 13:08:19 +0900

    test

--- yppicker/ViewController.swift
+++ yppicker/ViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import YPImagePicker
 
 class ViewController: UIViewController {
-
+    var items: [YPMediaPhoto] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        var config = YPImagePickerConfiguration()
+        config.library.maxNumberOfItems = 72
+        let picker = YPImagePicker(configuration: config)
+        picker.didFinishPicking { [unowned picker] items, cancelled in
+            for item in items {
+                switch item {
+                    
+                case .photo(let photo):
+                    self.items.append(photo)
+
+                    let image = photo.image
+                    print(self.items.count)
+                case .video(let video):
+                    print(video)
+                }
+            }
+            picker.dismiss(animated: true, completion: nil)
+        }
+        self.navigationController?.present(picker, animated: true)
     }
-
-
 }
 
